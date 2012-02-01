ca0f0575bf071780b947296d1fe3d01f0db4979d
Kjuly <kj@kjuly.com>
2012-02-01 21:40:03 +0800

    Create files: PoketchTabBar.h/m

---
+++ Pokemon/Views/PoketchTabBar.h
@@ -0,0 +1,13 @@
+//
+//  PoketchTabBar.h
+//  Pokemon
+//
+//  Created by Kaijie Yu on 2/1/12.
+//  Copyright (c) 2012 Kjuly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PoketchTabBar : UIView
+
+@end
---
+++ Pokemon/Views/PoketchTabBar.m
@@ -0,0 +1,31 @@
+//
+//  PoketchTabBar.m
+//  Pokemon
+//
+//  Created by Kaijie Yu on 2/1/12.
+//  Copyright (c) 2012 Kjuly. All rights reserved.
+//
+
+#import "PoketchTabBar.h"
+
+@implementation PoketchTabBar
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
