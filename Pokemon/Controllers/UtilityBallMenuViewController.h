02a4148773765f33e09e550c4a7d463a689af645
Kjuly <kj@kjuly.com>
2012-02-01 18:08:18 +0800

    Create file: UtilityBallMenuViewController.h/m

---
+++ Pokemon/Controllers/UtilityBallMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  UtilityBallMenuViewController.h
+//  Pokemon
+//
+//  Created by Kaijie Yu on 2/1/12.
+//  Copyright (c) 2012 Kjuly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UtilityBallMenuViewController : UIViewController
+
+@end
