3b66e5f4131c389d32d55897c9001341839a94cb
Kjuly <kj@kjuly.com>
2012-02-06 16:35:49 +0800

    Create: BagItemTableViewController.h/m

---
+++ Pokemon/Controllers/BagItemTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  BagItemTableViewController.h
+//  Pokemon
+//
+//  Created by Kaijie Yu on 2/6/12.
+//  Copyright (c) 2012 Kjuly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BagItemTableViewController : UITableViewController
+
+@end
