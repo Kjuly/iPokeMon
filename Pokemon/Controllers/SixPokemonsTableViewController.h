afbf216c47445f8fad0abbabba08b467a23cecee
Kjuly <kj@kjuly.com>
2012-02-09 22:19:30 +0800

    Create file: SixPokemonsTableViewController.h/m

---
+++ Pokemon/Controllers/SixPokemonsTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  SixPokemonsTableViewController.h
+//  Pokemon
+//
+//  Created by Kaijie Yu on 2/9/12.
+//  Copyright (c) 2012 Kjuly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CustomTableViewController.h"
+
+@interface SixPokemonsTableViewController : CustomTableViewController
+
+@end
