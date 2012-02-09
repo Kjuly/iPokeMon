cebfce32998da7fb1e63cfef2faf1ca3db02c693
Kjuly <kj@kjuly.com>
2012-02-09 15:55:15 +0800

    Optimize code for |CustomNavigationBar|

--- Pokemon/Views/CustomNavigationBar.m
+++ Pokemon/Views/CustomNavigationBar.m
@@ -31,44 +31,47 @@
 - (void)drawRect:(CGRect)rect
 {
   NSLog(@"*** CustomNavigationBar drawRect:");
-  if (navigationBarBackgroundImage_) {
-    float imageWidth = navigationBarBackgroundImage_.frame.size.width;
-    float imageHeight= navigationBarBackgroundImage_.frame.size.height;
-    [navigationBarBackgroundImage_.image drawInRect:CGRectMake(0.0f, 0.0f, imageWidth, imageHeight)];
-  }
-  else [super drawRect:rect];
   
-  // Create custom |backButton|
+  // Draw |navigationBarBackgroundImage_|
+  if (navigationBarBackgroundImage_)
+    [navigationBarBackgroundImage_.image drawInRect:navigationBarBackgroundImage_.frame];
+  else
+    [super drawRect:rect];
+  
+  // Create custom |backButton_|
   [self resetBackButton];
 }
 
-- (CGSize)sizeThatFits:(CGSize)size
-{
-  UIInterfaceOrientation orientation = (UIInterfaceOrientation)[[UIDevice currentDevice] orientation];
-  float newSizeWidth;
-  if ( UIInterfaceOrientationIsLandscape(orientation) ) newSizeWidth = 480.0f;
-  else newSizeWidth = 320.0f;
-  CGSize newSize = CGSizeMake(newSizeWidth, navigationBarBackgroundImage_.frame.size.height);
-  NSLog(@">>> new size: (%f, %f)", newSize.width, newSize.height);
-  return newSize;
+// Reset NavigationBar's size to container |navigationBarBackgroundImage_|
+// 
+////TODO: for Device Orientation:
+//
+//  UIInterfaceOrientation orientation = (UIInterfaceOrientation)[[UIDevice currentDevice] orientation];
+//  float newSizeWidth;
+//  if ( UIInterfaceOrientationIsLandscape(orientation) ) newSizeWidth = 480.0f;
+//  else newSizeWidth = 320.0f;
+//  CGSize newSize = CGSizeMake(newSizeWidth, navigationBarBackgroundImage_.frame.size.height);
+//  NSLog(@">>> new size: (%f, %f)", newSize.width, newSize.height);
+//  return newSize;
+//
+- (CGSize)sizeThatFits:(CGSize)size {
+  return self.navigationBarBackgroundImage.frame.size;
 }
 
-// Save the background image and call setNeedsDisplay to force a redraw
+// Save the background image to |navigationBarBackgroundImage_|,
+// If not shown, call |[self setNeedsDisplay];| to force a redraw.
 - (void)initNavigationBarWith:(UIImage *)backgroundImage
 {
-  NSLog(@"*** CustomNavigationBar initNavigationBarWith:");
   navigationBarBackgroundImage_ = [[UIImageView alloc]
                                    initWithFrame:CGRectMake(0.0f,
                                                             0.0f,
                                                             backgroundImage.size.width,
                                                             backgroundImage.size.height)];
   navigationBarBackgroundImage_.image = backgroundImage;
-  
-  [self setNeedsDisplay];
 }
 
 // Settings for |backButton|
-// With a custom back button, we have to provide the action. We simply pop the view controller
+// Provide the action for the custom |backButton|
 - (void)back:(id)sender {
   NSLog(@"popViewController");
   [self.navigationController popViewControllerAnimated:YES];
