228e8c7e6e0085997f4e8f3f2dd1dacb8e3abc58
Kjuly <kj@kjuly.com>
2012-02-09 23:21:09 +0800

    Create file: TrainerCardViewController.h/m

---
+++ Pokemon/Controllers/TrainerCardViewController.h
@@ -0,0 +1,13 @@
+//
+//  TrainerCardViewController.h
+//  Pokemon
+//
+//  Created by Kaijie Yu on 2/9/12.
+//  Copyright (c) 2012 Kjuly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TrainerCardViewController : UIViewController
+
+@end
---
+++ Pokemon/Controllers/TrainerCardViewController.m
@@ -0,0 +1,63 @@
+//
+//  TrainerCardViewController.m
+//  Pokemon
+//
+//  Created by Kaijie Yu on 2/9/12.
+//  Copyright (c) 2012 Kjuly. All rights reserved.
+//
+
+#import "TrainerCardViewController.h"
+
+@implementation TrainerCardViewController
+
+- (void)dealloc
+{
+  [super dealloc];
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+  self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+  if (self) {
+  }
+  return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+  // Releases the view if it doesn't have a superview.
+  [super didReceiveMemoryWarning];
+  
+  // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+// Implement loadView to create a view hierarchy programmatically, without using a nib.
+- (void)loadView
+{
+  [super loadView];
+  
+  UIView * view = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 320.0f, 420.0f)];
+  self.view = view;
+  [view release];
+}
+
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad
+{
+  [super viewDidLoad];
+}
+
+- (void)viewDidUnload
+{
+  [super viewDidUnload];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+  // Return YES for supported orientations
+  return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
