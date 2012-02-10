3b419eb181f0b14a81115ced0e9aaa5e158b5366
Kjuly <kj@kjuly.com>
2012-02-10 22:43:27 +0800

    Create file: PokedexTableViewCell.h/m

---
+++ Pokemon/Views/PokedexTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  PokedexTableViewCell.h
+//  Pokemon
+//
+//  Created by Kaijie Yu on 2/10/12.
+//  Copyright (c) 2012 Kjuly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PokedexTableViewCell : UITableViewCell
+
+@end
---
+++ Pokemon/Views/PokedexTableViewCell.m
@@ -0,0 +1,29 @@
+//
+//  PokedexTableViewCell.m
+//  Pokemon
+//
+//  Created by Kaijie Yu on 2/10/12.
+//  Copyright (c) 2012 Kjuly. All rights reserved.
+//
+
+#import "PokedexTableViewCell.h"
+
+@implementation PokedexTableViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+  self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+  if (self) {
+    // Initialization code
+  }
+  return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+  [super setSelected:selected animated:animated];
+  
+  // Configure the view for the selected state
+}
+
+@end
