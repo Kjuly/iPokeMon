599246e81a9da7a75a2a1ef18b3744be5ef3f904
Kjuly <kj@kjuly.com>
2012-02-10 13:57:26 +0800

    Create file: AccountSettingTableViewController.h/m

---
+++ Pokemon/Controllers/AccountSettingTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  AccountSettingTableViewController.h
+//  Pokemon
+//
+//  Created by Kaijie Yu on 2/10/12.
+//  Copyright (c) 2012 Kjuly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AccountSettingTableViewController : UITableViewController
+
+@end
