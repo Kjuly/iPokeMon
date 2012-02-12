56c31433a8e3acd6ae3366c9d8b0cf5013c63888
Kjuly <kj@kjuly.com>
2012-02-13 00:48:59 +0800

    Create file: PoketchStepsViewController.h/m

---
+++ Pokemon/Controllers/PoketchStepsViewController.h
@@ -0,0 +1,13 @@
+//
+//  PoketchStepsViewController.h
+//  Pokemon
+//
+//  Created by Kaijie Yu on 2/13/12.
+//  Copyright (c) 2012 Kjuly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PoketchStepsViewController : UIViewController
+
+@end
