dbdf0eafa6966130ac016751119219b7968becb8
Kjuly <kj@kjuly.com>
2012-02-16 00:01:36 +0800

    Create |TrainerModel|

---
+++ Pokemon/Models/TrainerModel.m
@@ -0,0 +1,21 @@
+//
+//  TrainerModel.m
+//  Pokemon
+//
+//  Created by Kaijie Yu on 2/15/12.
+//  Copyright (c) 2012 Kjuly. All rights reserved.
+//
+
+#import "TrainerModel.h"
+
+
+@implementation TrainerModel
+
+@dynamic id;
+@dynamic name;
+@dynamic pokedex;
+@dynamic money;
+@dynamic badges;
+@dynamic adventure_started;
+
+@end
