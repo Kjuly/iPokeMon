bf793881a1dd7c95b783335da599fe0657c83a15
Kjuly <kj@kjuly.com>
2012-02-17 15:47:09 +0800

    Create file: Pokemon+DataController.h/m

---
+++ Pokemon/Models/Pokemon+DataController.h
@@ -0,0 +1,17 @@
+//
+//  Pokemon+DataController.h
+//  Pokemon
+//
+//  Created by Kaijie Yu on 2/17/12.
+//  Copyright (c) 2012 Kjuly. All rights reserved.
+//
+
+#import "Pokemon.h"
+
+@interface Pokemon (DataController)
+
+// Pokedex
+
+// Six Pokemons
+
+@end
---
+++ Pokemon/Models/Pokemon+DataController.m
@@ -0,0 +1,13 @@
+//
+//  Pokemon+DataController.m
+//  Pokemon
+//
+//  Created by Kaijie Yu on 2/17/12.
+//  Copyright (c) 2012 Kjuly. All rights reserved.
+//
+
+#import "Pokemon+DataController.h"
+
+@implementation Pokemon (DataController)
+
+@end
