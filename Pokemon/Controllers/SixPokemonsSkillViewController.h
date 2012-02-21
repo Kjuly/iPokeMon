2c5e509cec1d95778b0c95e7f2f50fa8fc037992
Kjuly <kj@kjuly.com>
2012-02-22 02:35:38 +0800

    |Skills| tab view done

--- Pokemon/Controllers/SixPokemonsSkillViewController.h
+++ Pokemon/Controllers/SixPokemonsSkillViewController.h
@@ -9,5 +9,22 @@
 #import "SixPokemonsDetailViewController.h"
 
 @interface SixPokemonsSkillViewController : SixPokemonsDetailViewController
+{
+  PokemonInfoLabelView * hpLabelView_;
+  PokemonInfoLabelView * attackLabelView_;
+  PokemonInfoLabelView * defenseLabelView_;
+  PokemonInfoLabelView * spAttackLabelView_;
+  PokemonInfoLabelView * spDefenseLabelView_;
+  PokemonInfoLabelView * speedLabelView_;
+  PokemonInfoLabelView * abilityLabelView_;
+}
+
+@property (nonatomic, retain) PokemonInfoLabelView * hpLabelView;
+@property (nonatomic, retain) PokemonInfoLabelView * attackLabelView;
+@property (nonatomic, retain) PokemonInfoLabelView * defenseLabelView;
+@property (nonatomic, retain) PokemonInfoLabelView * spAttackLabelView;
+@property (nonatomic, retain) PokemonInfoLabelView * spDefenseLabelView;
+@property (nonatomic, retain) PokemonInfoLabelView * speedLabelView;
+@property (nonatomic, retain) PokemonInfoLabelView * abilityLabelView;
 
 @end
--- Pokemon/Controllers/SixPokemonsSkillViewController.m
+++ Pokemon/Controllers/SixPokemonsSkillViewController.m
@@ -10,8 +10,24 @@
 
 @implementation SixPokemonsSkillViewController
 
+@synthesize hpLabelView        = hpLabelView_;
+@synthesize attackLabelView    = attackLabelView_;
+@synthesize defenseLabelView   = defenseLabelView_;
+@synthesize spAttackLabelView  = spAttackLabelView_;
+@synthesize spDefenseLabelView = spDefenseLabelView_;
+@synthesize speedLabelView     = speedLabelView_;
+@synthesize abilityLabelView   = abilityLabelView_;
+
 - (void)dealloc
 {
+  [hpLabelView_        release];
+  [attackLabelView_    release];
+  [defenseLabelView_   release];
+  [spAttackLabelView_  release];
+  [spDefenseLabelView_ release];
+  [speedLabelView_     release];
+  [abilityLabelView_   release];
+  
   [super dealloc];
 }
 
@@ -29,19 +45,101 @@
 - (void)loadView
 {
   [super loadView];
+  
+  // Constants
+  CGFloat const imageHeight = 150.0f;
+  CGFloat const labelHeight = 30.0f;
+  
+  CGRect  const dataViewFrame           = CGRectMake(10.0f, imageHeight + 15.0f, 300.0f, 60.0f);
+  CGRect  const hpLabelViewFrame        = CGRectMake(0.0f, 0.0f, 140.0f, labelHeight);
+  CGRect  const attackLabelViewFrame    = CGRectMake(140.0f, 0.0f, 160.0f, labelHeight);
+  CGRect  const defenseLabelViewFrame   = CGRectMake(0.0f, labelHeight, 300.0f, labelHeight);
+  CGRect  const spAttackLabelViewFrame  = CGRectMake(0.0f, labelHeight * 2, 300.0f, labelHeight);
+  CGRect  const spDefenseLabelViewFrame = CGRectMake(0.0f, labelHeight * 3, 300.0f, labelHeight);
+  CGRect  const speedLabelViewFrame     = CGRectMake(0.0f, labelHeight * 4, 300.0f, labelHeight);
+  CGRect  const abilityLabelViewFrame   = CGRectMake(0.0f, labelHeight * 5, 300.0f, labelHeight);
+  
+  
+  ///Data View in Center
+  UIView * dataView = [[UIView alloc] initWithFrame:dataViewFrame];
+  
+  // HP
+  hpLabelView_ = [[PokemonInfoLabelView alloc] initWithFrame:hpLabelViewFrame hasValueLabel:YES];
+  [hpLabelView_.name setText:NSLocalizedString(@"kLabelHP", nil)];
+  [dataView addSubview:hpLabelView_];
+  
+  // Attack
+  attackLabelView_ = [[PokemonInfoLabelView alloc] initWithFrame:attackLabelViewFrame hasValueLabel:YES];
+  [attackLabelView_.name  setText:NSLocalizedString(@"kLabelAttack", nil)];
+  [dataView addSubview:attackLabelView_];
+  
+  // Defense
+  defenseLabelView_ = [[PokemonInfoLabelView alloc] initWithFrame:defenseLabelViewFrame hasValueLabel:YES];
+  [defenseLabelView_.name  setText:NSLocalizedString(@"kLabelDefense", nil)];
+  [dataView addSubview:defenseLabelView_];
+  
+  // Sp. Attack
+  spAttackLabelView_ = [[PokemonInfoLabelView alloc] initWithFrame:spAttackLabelViewFrame hasValueLabel:YES];
+  [spAttackLabelView_.name setText:NSLocalizedString(@"kLabelSpAttack", nil)];
+  [dataView addSubview:spAttackLabelView_];
+  
+  // Sp. Defense
+  spDefenseLabelView_ = [[PokemonInfoLabelView alloc] initWithFrame:spDefenseLabelViewFrame hasValueLabel:YES];
+  [spDefenseLabelView_.name setText:NSLocalizedString(@"kLabelSpDefense", nil)];
+  [dataView addSubview:spDefenseLabelView_];
+  
+  // Speed
+  speedLabelView_ = [[PokemonInfoLabelView alloc] initWithFrame:speedLabelViewFrame hasValueLabel:YES];
+  [speedLabelView_.name setText:NSLocalizedString(@"kLabelSpeed", nil)];
+  [dataView addSubview:speedLabelView_];
+  
+  // Ability
+  abilityLabelView_ = [[PokemonInfoLabelView alloc] initWithFrame:abilityLabelViewFrame hasValueLabel:YES];
+  [abilityLabelView_.name setText:NSLocalizedString(@"kLabelAbility", nil)];
+  [dataView addSubview:abilityLabelView_];
+  
+  // Add Data View to |self.view| & Release it
+  [self.view addSubview:dataView];
+  [dataView release];
 }
 
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad
 {
   [super viewDidLoad];
+  
+  NSArray * statsLeft;
+  NSArray * statsMax;
+  if ([self.pokemon.leftStats isKindOfClass:[NSString class]] || [self.pokemon.maxStats isKindOfClass:[NSString class]]) {
+    statsLeft = [self.pokemon.leftStats componentsSeparatedByString:@","];
+    statsMax  = [self.pokemon.maxStats  componentsSeparatedByString:@","];
+  }
+  else {
+    statsLeft = self.pokemon.leftStats;
+    statsMax  = self.pokemon.maxStats;
+  }
+  
+  [self.hpLabelView.value setText:[NSString stringWithFormat:@"%d/%d",
+                                  [[statsLeft objectAtIndex:0] intValue], [[statsMax objectAtIndex:0] intValue]]];
+  [self.attackLabelView.value setText:[statsMax objectAtIndex:1]];
+  [self.defenseLabelView.value setText:[statsMax objectAtIndex:2]];
+  [self.spAttackLabelView.value setText:[statsMax objectAtIndex:3]];
+  [self.spDefenseLabelView.value setText:[statsMax objectAtIndex:4]];
+  [self.speedLabelView.value setText:[statsMax objectAtIndex:5]];
+  [self.abilityLabelView.value setText:[self.pokemon.pokemon.ability1 stringValue]];
 }
 
 - (void)viewDidUnload
 {
   [super viewDidUnload];
-  // Release any retained subviews of the main view.
-  // e.g. self.myOutlet = nil;
+  
+  self.hpLabelView        = nil;
+  self.attackLabelView    = nil;
+  self.defenseLabelView   = nil;
+  self.spAttackLabelView  = nil;
+  self.spDefenseLabelView = nil;
+  self.speedLabelView     = nil;
+  self.abilityLabelView   = nil;
 }
 
 @end
