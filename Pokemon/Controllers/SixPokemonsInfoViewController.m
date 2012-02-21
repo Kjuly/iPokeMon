3dd499c33b050ffa77fb0acc54f069ed18919a66
Kjuly <kj@kjuly.com>
2012-02-22 01:57:35 +0800

    |SixPokemonsInfoViewController|'s view layout done

--- Pokemon/Controllers/SixPokemonsInfoViewController.m
+++ Pokemon/Controllers/SixPokemonsInfoViewController.m
@@ -10,8 +10,16 @@
 
 @implementation SixPokemonsInfoViewController
 
+@synthesize levelLabelView       = levelLabelView_;
+@synthesize expLabelView         = expLabelView_;
+@synthesize toNextLevelLabelView = toNextLevelLabelView_;
+
 - (void)dealloc
 {
+  [levelLabelView_       release];
+  [expLabelView_         release];
+  [toNextLevelLabelView_ release];
+  
   [super dealloc];
 }
 
@@ -31,74 +39,57 @@
   [super loadView];
   
   // Constants
-  CGFloat const imageHeight       = 150.0f;
+  CGFloat const imageHeight = 150.0f;
+  CGFloat const labelHeight = 30.0f;
   
-  CGFloat const labelHeight       = 30.0f;
-  CGFloat const labelWidth        = 80.0f;
-  CGFloat const valueHeight       = 30.0f;
-  CGFloat const valueWidth        = 300.0f - labelWidth;
+  CGRect  const dataViewFrame      = CGRectMake(10.0f, imageHeight + 15.0f, 300.0f, 60.0f);
+  CGRect  const levelLabelViewFrame = CGRectMake(0.0f, 0.0f, 140.0f, labelHeight);
+  CGRect  const genderLabelViewFrame = CGRectMake(140.0f, 0.0f, 160.0f, labelHeight);
+  CGRect  const typeLabelViewFrame = CGRectMake(0.0f, labelHeight, 300.0f, labelHeight);
+  CGRect  const expLabelViewFrame  = CGRectMake(0.0f, labelHeight * 2, 300.0f, labelHeight);
+  CGRect  const toNextLevelLabelViewFrame = CGRectMake(0.0f, labelHeight * 3, 300.0f, labelHeight);
   
-  CGRect  const dataViewFrame     = CGRectMake(10.0f, imageHeight + 15.0f, 300.0f, 60.0f);
-  CGRect  const descriptionFrame  = CGRectMake(10.0f,
-                                               imageHeight + dataViewFrame.size.height + 20.0f,
-                                               300.0f,
-                                               130.0f);
   
   // Base information for Pokemon
   Pokemon * pokemonBaseInfo = self.pokemon.pokemon;
   
-  
   ///Data View in Center
   UIView * dataView = [[UIView alloc] initWithFrame:dataViewFrame];
   
-  // Species
-  UILabel * speciesLabel = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 0.0f, labelWidth, labelHeight)];
-  UILabel * speciesValue = [[UILabel alloc] initWithFrame:CGRectMake(labelWidth, 0.0f, valueWidth, valueHeight)];
-  [speciesLabel setBackgroundColor:[UIColor clearColor]];
-  [speciesValue setBackgroundColor:[UIColor clearColor]];
-  [speciesLabel setTextColor:[GlobalRender textColorBlue]];
-  [speciesLabel setFont:[GlobalRender textFontBoldInSizeOf:16.0f]];
-  [speciesValue setFont:[GlobalRender textFontBoldInSizeOf:16.0f]];
-  [speciesLabel setTextAlignment:UITextAlignmentRight];
-  [speciesValue setTextAlignment:UITextAlignmentLeft];
-  [speciesLabel setText:NSLocalizedString(@"kLabelSpecies", nil)];
-  [speciesValue setText:[pokemonBaseInfo.species stringValue]];
-  [dataView addSubview:speciesLabel];
-  [dataView addSubview:speciesValue];
-  [speciesLabel release];
-  [speciesValue release];
+  // Level
+  levelLabelView_ = [[PokemonInfoLabelView alloc] initWithFrame:levelLabelViewFrame hasValueLabel:YES];
+  [levelLabelView_.name setText:NSLocalizedString(@"kLabelLevel", nil)];
+  [dataView addSubview:levelLabelView_];
+  
+  // Gender
+  PokemonInfoLabelView * genderLabelView = [[PokemonInfoLabelView alloc] initWithFrame:genderLabelViewFrame hasValueLabel:YES];
+  [genderLabelView.name  setText:NSLocalizedString(@"kLabelGender", nil)];
+  [genderLabelView.value setText:[self.pokemon.gender intValue] ? @"M" : @"F"];
+  [dataView addSubview:genderLabelView];
+  [genderLabelView release];
   
   // Type
-  UILabel * typeLabel = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, labelHeight, labelWidth, labelHeight)];
-  UILabel * typeValue = [[UILabel alloc] initWithFrame:CGRectMake(labelWidth, labelHeight, valueWidth, valueHeight)];
-  [typeLabel setBackgroundColor:[UIColor clearColor]];
-  [typeValue setBackgroundColor:[UIColor clearColor]];
-  [typeLabel setTextColor:[GlobalRender textColorBlue]];
-  [typeLabel setFont:[GlobalRender textFontBoldInSizeOf:16.0f]];
-  [typeValue setFont:[GlobalRender textFontBoldInSizeOf:16.0f]];
-  [typeLabel setTextAlignment:UITextAlignmentRight];
-  [typeValue setTextAlignment:UITextAlignmentLeft];
-  [typeLabel setText:NSLocalizedString(@"kLabelType", nil)];
-  [typeValue setText:[pokemonBaseInfo.type1 stringValue]];
-  [dataView addSubview:typeLabel];
-  [dataView addSubview:typeValue];
-  [typeLabel release];
-  [typeValue release];
+  PokemonInfoLabelView * typeLabelView = [[PokemonInfoLabelView alloc] initWithFrame:typeLabelViewFrame hasValueLabel:YES];
+  [typeLabelView.name  setText:NSLocalizedString(@"kLabelType", nil)];
+  [typeLabelView.value setText:[pokemonBaseInfo.type1 stringValue]];
+  [dataView addSubview:typeLabelView];
+  [typeLabelView release];
+  
+  // EXP
+  expLabelView_ = [[PokemonInfoLabelView alloc] initWithFrame:expLabelViewFrame hasValueLabel:YES];
+  [expLabelView_.name setFont:[GlobalRender textFontBoldInSizeOf:12.0f]];
+  [expLabelView_.name setText:NSLocalizedString(@"kLabelEXP", nil)];
+  [dataView addSubview:expLabelView_];
+  
+  // To Next Level
+  toNextLevelLabelView_ = [[PokemonInfoLabelView alloc] initWithFrame:toNextLevelLabelViewFrame hasValueLabel:YES];
+  [toNextLevelLabelView_.name setFont:[GlobalRender textFontBoldInSizeOf:12.0f]];
+  [toNextLevelLabelView_.name setText:NSLocalizedString(@"kLabelToNextLevel", nil)];
+  [dataView addSubview:toNextLevelLabelView_];
   
   // Add Data View to |self.view| & Release it
   [self.view addSubview:dataView];
   [dataView release];
-  
-  
-  ///Description
-  UITextView * descriptionField = [[UITextView alloc] initWithFrame:descriptionFrame];
-  [descriptionField setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"PokemonDetailDescriptionBackground.png"]]];
-  [descriptionField setOpaque:NO];
-  [descriptionField setEditable:NO];
-  [descriptionField setFont:[GlobalRender textFontNormalInSizeOf:14.0f]];
-  [descriptionField setText:pokemonBaseInfo.info];
-  [self.view addSubview:descriptionField];
-  [descriptionField release];
 }
 
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
@@ -110,6 +101,19 @@
 - (void)viewDidUnload
 {
   [super viewDidUnload];
+  
+  self.levelLabelView       = nil;
+  self.expLabelView         = nil;
+  self.toNextLevelLabelView = nil;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+  [super viewWillAppear:animated];
+  
+  [self.levelLabelView.value       setText:[self.pokemon.level stringValue]];
+  [self.expLabelView.value         setText:[self.pokemon.currEXP stringValue]];
+  [self.toNextLevelLabelView.value setText:[self.pokemon.toNextLevel stringValue]];
 }
 
 @end
