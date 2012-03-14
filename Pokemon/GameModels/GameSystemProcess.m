da27d003abe5b05ed9563bfd273e169dd3a5cff7
Kjuly <kj@kjuly.com>
2012-03-14 23:56:18 +0800

    Add 'Move Effect Code Description' for Move

--- Pokemon/GameModels/GameSystemProcess.m
+++ Pokemon/GameModels/GameSystemProcess.m
@@ -115,6 +115,117 @@ static GameSystemProcess * gameSystemProcess = nil;
   delayTime_ = 0;
 }
 
+/*
+ Move Effect Code Description
+ 
+ 00 - Normal Damage (e.g. Pound)
+ 01 - Puts Enemy to Sleep, no damage
+ 02 - 29.8% Chance of Poison (e.g. Poison Sting)
+ 03 - Absorbs half of damage inflicted (e.g. Mega Drain)
+ 04 - 9.8% Chance of Burn (e.g. Ember)
+ 05 - 9.8% Chance of Freeze (e.g. Ice Beam)
+ 06 - 9.8% Chance of Paralyze (e.g. Thunderbolt)
+ 07 - Opponent's defense is halved during attack, user faints (e.g. Explosion)
+ 08 - Absorbs half of damage inflicted, only works if opponent is sleeping (e.g. Dream Eater)
+ 09 - User uses last attack used (e.g. Mirror Move)
+ 0A - Raises Attack UP 1 Stage (e.g. Meditate)
+ 0B - Raises Defense UP 1 Stage (e.g. Harden)
+ 0C - Raises Speed UP 1 Stage (e.g. None)
+ 0D - Raises Special UP 1 Stage (e.g. Growth)
+ 0E - Raises Accuracy UP 1 Stage (e.g. None)
+ 0F - Raises Evasion UP 1 Stage (e.g. Double Team)
+ 10 - Gain money after battle (User's Level*2*Number of uses) (e.g. Pay Day)
+ 11 - 99.6% of hitting the opponent (Ignores Stat changes, hits flying/digging opponents) (e.g. Swift)
+ 12 - Lowers Attack DOWN 1 Stage (Probability = Hit Chance) (e.g. Growl)
+ 13 - Lowers Defense DOWN 1 Stage (Probability = Hit Chance) (e.g. Leer)
+ 14 - Lowers Speed DOWN 1 Stage (Probability = Hit Chance) (e.g. String Shot)
+ 15 - Lowers Special DOWN 1 Stage (Probability = Hit Chance) (e.g. None)
+ 16 - Lowers Accuracy DOWN 1 Stage (Probability = Hit Chance) (e.g. Sand Attack)
+ 17 - Lowers Evasion DOWN 1 Stage (Probability = Hit Chance) (e.g. None)
+ 18 - Changes user's type to match Opponent's (e.g. Conversion)
+ 19 - Removes all stat changes, returns opponent's status to Normal (e.g. Haze)
+ 1A - User doesn't attack for 2-3 turns, then returns double the damage taken (e.g. Bide)
+ 1B - Attacks 2-3 times, afterwards user becomes Confused (e.g. Thrash)
+ 1C - Ends battle (Probability = Hit Chance) (e.g. Teleport)
+ 1D - Attacks 2-5 Times in one turn (e.g. Fury Swipes)
+ 1E - Attacks 2-5 Turns (e.g. None)
+ 1F - 9.8% Chance of Flinch (e.g. Bite)
+ 20 - Chance of putting opponent to Sleep for 1-7 Turns (Probability = Hit Chance) (e.g. Sleep Powder)
+ 21 - 40% Chance of Poison (e.g. Smog)
+ 22 - 30.1% Chance of Burn (e.g. Fire Blast)
+ 23 - 30.1% Chance of Freeze (e.g. None)
+ 24 - 30.1% Chance of Paralyze (e.g. Body Slam)
+ 25 - 30.1% Chance of Flinch (e.g. Stomp)
+ 26 - OHKO (e.g. Fissure)
+ 27 - Charges for one turn, attacks on the next (e.g. Razor Wind)
+ 28 - Deals set damage, leaves 1HP (e.g. Super Fang)
+ 29 - Deals set damage (Special equation depending on move?) (e.g. Seismic Toss, Dragon Rage, Psywave)
+ 2A - Attack 2-5 times, prevents opponent from attacking (e.g. Wrap)
+ 2B - Charges for one turn, attacks on the next (Can't be hit during) (e.g. Fly)
+ 2C - Attacks 2 times (e.g. Double Kick)
+ 2D - If user misses, 1 damage to user (e.g. Jump Kick)
+ 2E - Evade effects that lower stats (e.g. Mist)
+ 2F - No effect if user is faster than opponent. If user is slower, Critical Hit ratio goes to 0/511 (In other words, completely broken) (e.g. Focus Energy)
+ 30 - User receives Recoil damage equal to 1/4 the damage dealed (e.g. Take Down)
+ 31 - Chance of Confusion (Probability = Hit Chance) (e.g. Supersonic)
+ 32 - Raises Attack UP 2 Stages (e.g. Swords Dance)
+ 33 - Raises Defense UP 2 Stages (e.g. Barrier)
+ 34 - Raises Speed UP 2 Stages (e.g. Agility)
+ 35 - Raises Special UP 2 Stages (e.g. Amnesia)
+ 36 - Raises Accuracy UP 2 Stages (e.g. None)
+ 37 - Raises Evasion UP 2 Stages (e.g. None)
+ 38 - User Recovers HP (Excluding rest HP = Max HP/2) (Examples: Recover, Rest)
+ 39 - Transform into opponent, inheriting all stats (e.g. Transform)
+ 3A - Lowers Attack DOWN 2 Stages (Probability = Hit Chance) (e.g. None)
+ 3B - Lowers Defense DOWN 2 Stages (Probability = Hit Chance) (e.g. Screech)
+ 3C - Lowers Speed DOWN 2 Stages (Probability = Hit Chance) (e.g. None)
+ 3D - Lowers Special DOWN 2 Stages (Probability = Hit Chance) (e.g. None)
+ 3E - Lowers Accuracy DOWN 2 Stages (Probability = Hit Chance) (e.g. None)
+ 3F - Lowers Evasion DOWN 2 Stages (Probability = Hit Chance) (e.g. None)
+ 40 - Doubles Special when being attacked (e.g. Light Screen)
+ 41 - Doubles Defense when being attacked (e.g. Reflect)
+ 42 - Chance to Poison (Probability = Hit Chance) (e.g. Poison Gas)
+ 43 - Chance to Paralyze (Probability = Hit Chance) (e.g. Stun Spore)
+ 44 - 9.8% Chance of lowering Attack DOWN 1 Stage (e.g. Aurora Beam)
+ 45 - 9.8% Chance of lowering Defence DOWN 1 Stage (e.g. Acid)
+ 46 - 9.8% Chance of lowering Speed DOWN 1 Stage (e.g. Bubble)
+ 47 - 29.8% Chance of lowering Special DOWN 1 Stage (e.g. Psychic)
+ 48 - 9.8%? Chance of lowering Accuracy DOWN 1 Stage (e.g. None)
+ 49 - 9.8%? Chance of lowering Evasion DOWN 1 Stage (e.g. None)
+ 4A - None
+ 4B - None
+ 4C - 9.8% Chance of Confusion (e.g. Confusion)
+ 4D - Attacks 2 times, 19.8% chance of Poison (e.g. Twineedle)
+ 4E - Game crashes after attack
+ 4F - Creates close of user, user HP decreases about 1/4 (e.g. Substitute)
+ 50 - User can't attack second turn if opponent doesn't faint (e.g. Hyper Beam)
+ 51 - User's attack raises UP 1 stage each time user is hit, can't be cancelled (e.g. Rage)
+ 52 - Move is replaced by chosen move of opponent's for the rest of the battle or until switched (e.g. Mimic)
+ 53 - Uses a random attack (e.g. Metronome)
+ 54 - Absorbs HP each turn (About 1/16 of enemy HP) (e.g. Leech Seed)
+ 55 - No effect (e.g. Splash)
+ 56 - Disables a random attack of the opponent for 2-5 turns (e.g. Disable)
+ 57-65 - None?
+ 66 - Teleports you to the last used Pokemon Center. If used in battle, the battle restarts outside of the Center.
+ 67-76 - None?
+ 77 - Unknown Effect
+ 78-7F - None?
+ 80 - Freezes
+ 81-82- None?
+ 83 - Absorbs HP
+ 84 - None?
+ 85 - May Paralyze opponent
+ 86 - May Paralyze opponent
+ 87 - User faints after attacking
+ 88 - Absorbs HP
+ 89 - Crashes
+ 8A - Unknown Effect ("Nothing Happened" after attack)
+ 8B-8F - None?
+ 90 - Same as 10 (e.g. Pay Day)
+ 91-98 - None?
+ 99 - All status changes eliminated
+ Most past 99 are all glitched.
+ */
 - (void)applyMove
 {
   NSString * moveTarget;
