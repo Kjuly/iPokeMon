0dfc82b9ab389cf2644d37f1102690efb85e86d8
Kjuly <kj@kjuly.com>
2012-03-23 14:28:54 +0800

    Add |LoginViewController|

---
+++ Pokemon/Controllers/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Pokemon
+//
+//  Created by Kaijie Yu on 3/23/12.
+//  Copyright (c) 2012 Kjuly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ Pokemon/Controllers/LoginViewController.m
@@ -0,0 +1,54 @@
+//
+//  LoginViewController.m
+//  Pokemon
+//
+//  Created by Kaijie Yu on 3/23/12.
+//  Copyright (c) 2012 Kjuly. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@implementation LoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+  self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+  if (self) {
+    // Custom initialization
+  }
+  return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+  // Releases the view if it doesn't have a superview.
+  [super didReceiveMemoryWarning];
+  
+  // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+// Implement loadView to create a view hierarchy programmatically, without using a nib.
+- (void)loadView
+{
+}
+
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad
+{
+  [super viewDidLoad];
+}
+
+- (void)viewDidUnload
+{
+  [super viewDidUnload];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+  // Return YES for supported orientations
+  return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
