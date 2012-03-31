63cbe213b4db99a8d875ef47d5fb5085081fcb25
Kjuly <kj@kjuly.com>
2012-03-31 22:39:18 +0800

    Add SHA & MD5 code snippets to |NSString+Encrypt|

--- Pokemon/GlobalFiles/Categories/NSString+Encrypt.m
+++ Pokemon/GlobalFiles/Categories/NSString+Encrypt.m
@@ -30,4 +30,36 @@
   return output;
 }
 
+/*
+// SHA1
+- (NSString*) sha1:(NSString*)input {
+  const char *cstr = [input cStringUsingEncoding:NSUTF8StringEncoding];
+  NSData *data = [NSData dataWithBytes:cstr length:input.length];
+  
+  uint8_t digest[CC_SHA1_DIGEST_LENGTH];
+  
+  CC_SHA1(data.bytes, data.length, digest);
+  
+  NSMutableString* output = [NSMutableString stringWithCapacity:CC_SHA1_DIGEST_LENGTH * 2];
+  
+  for(int i = 0; i < CC_SHA1_DIGEST_LENGTH; i++)
+    [output appendFormat:@"%02x", digest[i]];
+  
+  return output;
+}
+
+// MD5
+- (NSString *)md5:(NSString *)input {
+  const char *cStr = [input UTF8String];
+  unsigned char digest[16];
+  CC_MD5( cStr, strlen(cStr), digest ); // This is the md5 call
+  
+  NSMutableString *output = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
+  
+  for(int i = 0; i < CC_MD5_DIGEST_LENGTH; i++)
+    [output appendFormat:@"%02x", digest[i]];
+  
+  return  output;
+}*/
+
 @end
