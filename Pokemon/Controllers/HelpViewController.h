cc0be215835f85fd4ac565da1c6229fb3cc69235
Kjuly <kj@kjuly.com>
2012-04-03 22:05:55 +0800

    Add files:NewTrainerGuideViewController.h/m & HelpViewController.h/m

---
+++ Pokemon/Controllers/HelpViewController.h
@@ -0,0 +1,13 @@
+//
+//  HelpViewController.h
+//  Pokemon
+//
+//  Created by Kaijie Yu on 4/3/12.
+//  Copyright (c) 2012 Kjuly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelpViewController : UIViewController
+
+@end
---
+++ Pokemon/Controllers/NewTrainerGuideViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewTrainerGuideViewController.h
+//  Pokemon
+//
+//  Created by Kaijie Yu on 4/3/12.
+//  Copyright (c) 2012 Kjuly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewTrainerGuideViewController : UIViewController
+
+@end
