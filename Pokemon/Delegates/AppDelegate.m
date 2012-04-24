bb66d8677af8e31e72a884fb410efd45dc9736d1
Kjuly <kj@kjuly.com>
2012-04-25 01:46:57 +0800

    Modify AppDelegate.m

--- Pokemon/Delegates/AppDelegate.m
+++ Pokemon/Delegates/AppDelegate.m
@@ -8,7 +8,6 @@
 
 #import "AppDelegate.h"
 
-#import "GlobalConstants.h"
 #import "GlobalNotificationConstants.h"
 #import "MainViewController.h"
 
@@ -30,8 +29,7 @@
 @synthesize managedObjectModel = __managedObjectModel;
 @synthesize persistentStoreCoordinator = __persistentStoreCoordinator;
 
-- (void)dealloc
-{
+- (void)dealloc {
   [_window release];
   [__managedObjectContext release];
   [__managedObjectModel release];
@@ -39,8 +37,7 @@
   [super dealloc];
 }
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   // User Preferences
   // If not exists, load keyValues from |Settings.bundle| to |NSUserDefaults|
   NSString * appVersion = [[NSUserDefaults standardUserDefaults] objectForKey:kUDKeyAboutVersion];
@@ -120,52 +117,45 @@
   return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application
-{
+- (void)applicationWillResignActive:(UIApplication *)application {
   /*
    Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
    Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
    */
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
+- (void)applicationDidEnterBackground:(UIApplication *)application {
   /*
    Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
    If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
    */
   NSLog(@"Application entered background state...");
+  [self saveContext];
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
+- (void)applicationWillEnterForeground:(UIApplication *)application {
   /*
    Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
    */
   application.applicationIconBadgeNumber = 0;
 }
 
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
+- (void)applicationDidBecomeActive:(UIApplication *)application {
   /*
    Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
    */
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application
-{
+- (void)applicationWillTerminate:(UIApplication *)application {
   // Saves changes in the application's managed object context before the application terminates.
   [self saveContext];
 }
 
-- (void)saveContext
-{
+- (void)saveContext {
   NSError *error = nil;
   NSManagedObjectContext *managedObjectContext = self.managedObjectContext;
-  if (managedObjectContext != nil)
-  {
-    if ([managedObjectContext hasChanges] && ![managedObjectContext save:&error])
-    {
+  if (managedObjectContext != nil) {
+    if ([managedObjectContext hasChanges] && ![managedObjectContext save:&error]) {
       /*
        Replace this implementation with code to handle the error appropriately.
        
@@ -183,16 +173,12 @@
  Returns the managed object context for the application.
  If the context doesn't already exist, it is created and bound to the persistent store coordinator for the application.
  */
-- (NSManagedObjectContext *)managedObjectContext
-{
+- (NSManagedObjectContext *)managedObjectContext {
   if (__managedObjectContext != nil)
-  {
     return __managedObjectContext;
-  }
   
   NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
-  if (coordinator != nil)
-  {
+  if (coordinator != nil) {
     __managedObjectContext = [[NSManagedObjectContext alloc] init];
     [__managedObjectContext setPersistentStoreCoordinator:coordinator];
   }
@@ -203,12 +189,10 @@
  Returns the managed object model for the application.
  If the model doesn't already exist, it is created from the application's model.
  */
-- (NSManagedObjectModel *)managedObjectModel
-{
+- (NSManagedObjectModel *)managedObjectModel {
   if (__managedObjectModel != nil)
-  {
     return __managedObjectModel;
-  }
+  
   NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"Pokemon" withExtension:@"momd"];
   __managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
   return __managedObjectModel;
@@ -218,15 +202,11 @@
  Returns the persistent store coordinator for the application.
  If the coordinator doesn't already exist, it is created and the application's store added to it.
  */
-- (NSPersistentStoreCoordinator *)persistentStoreCoordinator
-{
+- (NSPersistentStoreCoordinator *)persistentStoreCoordinator {
   if (__persistentStoreCoordinator != nil)
-  {
     return __persistentStoreCoordinator;
-  }
   
   NSURL * storeURL;
-  
 #ifndef DEBUG_PREINIT_POPULATE_DATA
   // Generate Path for |Pokemon.sqlite| in Document
   NSArray * paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
@@ -250,8 +230,7 @@
   
   NSError *error = nil;
   __persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
-  if (![__persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error])
-  {
+  if (![__persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error]) {
     /*
      Replace this implementation with code to handle the error appropriately.
      
@@ -287,22 +266,19 @@
 /**
  Returns the URL to the application's Documents directory.
  */
-- (NSURL *)applicationDocumentsDirectory
-{
+- (NSURL *)applicationDocumentsDirectory {
   return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
 }
 
 #pragma mark - Notification
 
 // Remote Notification
-- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
-{
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
   NSLog(@"--- AppDelegate didReceiveRemoteNotification:");
 }
 
 // Local Notification
-- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification
-{
+- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification {
   NSLog(@"Background to Foreground, Run App after User Pressed the button");
   application.applicationIconBadgeNumber = 0;  
   [[NSNotificationCenter defaultCenter] postNotificationName:kPMNPokemonAppeared
