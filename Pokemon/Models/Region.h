dce531339d87932ead4c6a709bfbdebe542057bb
Kjuly <kj@kjuly.com>
2012-05-05 16:19:28 +0800

    New Model:|Region|, reinit Pokemon.sqlite3

---
+++ Pokemon/Models/Region.h
@@ -0,0 +1,21 @@
+//
+//  Region.h
+//  iPokemon
+//
+//  Created by Kaijie Yu on 5/5/12.
+//  Copyright (c) 2012 Kjuly. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Region : NSManagedObject
+
+@property (nonatomic, retain) NSString * code;
+@property (nonatomic, retain) NSString * countryCode;
+@property (nonatomic, retain) NSString * administrativeArea;
+@property (nonatomic, retain) NSString * locality;
+@property (nonatomic, retain) NSString * subLocality;
+
+@end
---
+++ Pokemon/Models/Region.m
@@ -0,0 +1,20 @@
+//
+//  Region.m
+//  iPokemon
+//
+//  Created by Kaijie Yu on 5/5/12.
+//  Copyright (c) 2012 Kjuly. All rights reserved.
+//
+
+#import "Region.h"
+
+
+@implementation Region
+
+@dynamic code;
+@dynamic countryCode;
+@dynamic administrativeArea;
+@dynamic locality;
+@dynamic subLocality;
+
+@end
