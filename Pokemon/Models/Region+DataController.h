caa7f188432b899f9046977437705ec0740ce6af
Kjuly <kj@kjuly.com>
2012-05-05 16:21:47 +0800

    New files:Region+DataController.h/m

---
+++ Pokemon/Models/Region+DataController.h
@@ -0,0 +1,13 @@
+//
+//  Region+DataController.h
+//  iPokemon
+//
+//  Created by Kaijie Yu on 5/5/12.
+//  Copyright (c) 2012 Kjuly. All rights reserved.
+//
+
+#import "Region.h"
+
+@interface Region (DataController)
+
+@end
---
+++ Pokemon/Models/Region+DataController.m
@@ -0,0 +1,13 @@
+//
+//  Region+DataController.m
+//  iPokemon
+//
+//  Created by Kaijie Yu on 5/5/12.
+//  Copyright (c) 2012 Kjuly. All rights reserved.
+//
+
+#import "Region+DataController.h"
+
+@implementation Region (DataController)
+
+@end
