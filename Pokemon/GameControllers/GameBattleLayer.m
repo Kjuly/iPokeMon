f6ff0901df1421fe7e02ed7607fe1e69b86534ba
Kjuly <kj@kjuly.com>
2012-05-07 00:19:12 +0800

    No rect for |BattleScenePMPoint|

--- Pokemon/GameControllers/GameBattleLayer.m
+++ Pokemon/GameControllers/GameBattleLayer.m
@@ -202,9 +202,11 @@
   [self addChild:self.background];
   
   // Pokemons' Points
-  CGRect pokemonPointFrame = CGRectMake(0.f, 0.f, kGameBattlePlayerPMPointWidth, kGameBattlePlayerPMPointHeight);
-  self.playerPokemonPoint = [CCSprite spriteWithFile:kPMINBattleScenePMPoint rect:pokemonPointFrame];
-  self.enemyPokemonPoint  = [CCSprite spriteWithFile:kPMINBattleScenePMPoint rect:pokemonPointFrame];
+//  CGRect pokemonPointFrame = CGRectMake(0.f, 0.f, kGameBattlePlayerPMPointWidth, kGameBattlePlayerPMPointHeight);
+  self.playerPokemonPoint = [CCSprite spriteWithFile:kPMINBattleScenePMPoint];
+  self.enemyPokemonPoint  = [CCSprite spriteWithFile:kPMINBattleScenePMPoint];
+//  self.playerPokemonPoint = [CCSprite spriteWithFile:kPMINBattleScenePMPoint rect:pokemonPointFrame];
+//  self.enemyPokemonPoint  = [CCSprite spriteWithFile:kPMINBattleScenePMPoint rect:pokemonPointFrame];
   [self.playerPokemonPoint setPosition:ccp(kGameBattlePlayerPMPointPosX, kGameBattlePlayerPMPointPosY)];
   [self.enemyPokemonPoint  setPosition:ccp(kGameBattleEnemyPMPointPosX,  kGameBattleEnemyPMPointPosY)];
   [self addChild:self.playerPokemonPoint];
