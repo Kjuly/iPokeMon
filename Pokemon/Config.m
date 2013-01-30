66c6fcb06d94d65bed01770362e5278dab38386a
Kjuly <kj@kjuly.com>
2013-01-30 12:08:02 +0800

    Track Config.h/m

---
+++ Pokemon/Config.m
@@ -0,0 +1,46 @@
+//
+//  Config.m
+//  iPokeMon
+//
+//  Created by Kjuly on 1/18/13.
+//  Copyright (c) 2013 Kjuly. All rights reserved.
+//
+
+#import "Config.h"
+
+/*
+ *pragma mark - OAuth Configuration
+ */
+// Client Identifier to match C/S,
+//   any string value is okay, but alpha is better
+//   e.g. "iPokeMonClientIdentifier"
+NSString * const kOAuthClientIdentifier = @"";
+//
+// Google
+// Client ID for Google's Authentication
+//   e.g. "123456789012.apps.googleusercontent.com"
+NSString * const kOAuthGoogleClientID = @"";
+//
+// Client Secret for Google's Authentication
+//   e.g. "O0vXxXPUR7xXxYKz7xX6SLQ"
+NSString * const kOAuthGoogleClientSecret = @"";
+//
+// Item name to be stored in keychain for Google OpenID
+// Any string value is okay, but alpha is better
+//   e.g. "PMOAuth2_Google"
+NSString * const kOAuthGoogleKeychainItemName = @"";
+//
+// Scope for Google+ API,
+NSString * const kOAuthGoogleScope = @"https://www.googleapis.com/auth/plus.me";
+
+/*
+ * In-App Purchase Configuration
+ */
+// Tiers
+NSString * const kIAPCurrencyTier1 = @"<your_identifier>.tier1";
+NSString * const kIAPCurrencyTier2 = @"<your_identifier>.tier2";
+NSString * const kIAPCurrencyTier3 = @"<your_identifier>.tier3";
+
+@implementation Config
+
+@end
