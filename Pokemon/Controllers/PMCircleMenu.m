4386744263cfa8eb115e4f41869674ab7f5a2251
Kjuly <kj@kjuly.com>
2013-02-02 22:15:57 +0800

    Hide navigation bar animated when back to root view

--- Pokemon/Controllers/PMCircleMenu.m
+++ Pokemon/Controllers/PMCircleMenu.m
@@ -10,23 +10,15 @@
 
 @implementation PMCircleMenu
 
-#pragma mark - Public Methods
-
 // Change |centerMainButton_|'s status in main view
 - (void)changeCenterMainButtonStatusToMove:(CenterMainButtonStatus)centerMainButtonStatus {
   // |centerMainButtonStatus : 1|, move |centerMainButton_| to view bottom
-  NSDictionary * userInfo =
-    [[NSDictionary alloc] initWithObjectsAndKeys:
-      [NSNumber numberWithInt:centerMainButtonStatus], @"centerMainButtonStatus", nil];
+  NSDictionary * userInfo = [[NSDictionary alloc] initWithObjectsAndKeys:
+                             [NSNumber numberWithInt:centerMainButtonStatus], @"centerMainButtonStatus", nil];
   [[NSNotificationCenter defaultCenter] postNotificationName:kPMNChangeCenterMainButtonStatus
                                                       object:self
                                                     userInfo:userInfo];
   [userInfo release];
-  
-  // If change |centerMainButton_|'s status to normal,
-  //   recover the state to expand
-  if (centerMainButtonStatus == kCenterMainButtonStatusNormal)
-    [self updateButtonsLayoutForState:kKYCircleMenuStateExpand animated:YES];
 }
 
 @end
