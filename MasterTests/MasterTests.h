595a115bf893b5ea1fc4505d637ed0a99e808749
Kjuly <kj@kjuly.com>
2013-02-27 14:13:51 +0800

    Rename PokemonTests to MasterTests

--- MasterTests/MasterTests.h
+++ MasterTests/MasterTests.h
@@ -1,6 +1,6 @@
 //
-//  PokemonTests.h
-//  PokemonTests
+//  MasterTests.h
+//  MasterTests
 //
 //  Created by Kaijie Yu on 1/30/12.
 //  Copyright (c) 2012 Kjuly. All rights reserved.
@@ -8,6 +8,6 @@
 
 #import <SenTestingKit/SenTestingKit.h>
 
-@interface PokemonTests : SenTestCase
+@interface MasterTests : SenTestCase
 
 @end
---
+++ MasterTests/MasterTests.m
@@ -0,0 +1,32 @@
+//
+//  MasterTests.m
+//  MasterTests
+//
+//  Created by Kaijie Yu on 1/30/12.
+//  Copyright (c) 2012 Kjuly. All rights reserved.
+//
+
+#import "MasterTests.h"
+
+@implementation MasterTests
+
+- (void)setUp
+{
+  [super setUp];
+  
+  // Set-up code here.
+}
+
+- (void)tearDown
+{
+  // Tear-down code here.
+  
+  [super tearDown];
+}
+
+- (void)testExample
+{
+  STFail(@"Unit tests are not implemented yet in PokemonTests");
+}
+
+@end
